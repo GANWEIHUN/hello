1eb0a54f05f6c4090d6099e9160e71711c8bc846
tomato <ganweihun@yunksj.com>
2023-08-10 10:34:57 +0800

    添加注释

--- src/test/java/MyTest.java
+++ src/test/java/MyTest.java
@@ -541,6 +541,11 @@ public class MyTest {
 
     private void testStack() {
         System.out.println(Thread.currentThread().getStackTrace()[1].getMethodName());
+        //  栈（Stack）是一种后进先出（LIFO）的数据结构，操作栈的元素的方法有：
+        //• 把元素压栈：push(E)；
+        //• 把栈顶的元素“弹出”：pop(E)；
+        //• 取栈顶元素但不弹出：peek(E)。
+        //  在Java中，我们用Deque可以实现Stack的功能，注意只调用push()/pop()/peek()方法，避免调用Deque的其他方法。
         Deque<String> stack2 = new LinkedList<>();
         stack2.push("hello");
         stack2.push("haha");
